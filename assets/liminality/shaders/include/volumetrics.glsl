62a7a17239b2197d147be9295db5385ccfdbe6d8
Lunar Sundae <dapimasta@gmail.com>
2026-02-22 18:48:54 -0800

    modified volumetrics.glsl

--- assets/liminality/shaders/include/volumetrics.glsl
+++ assets/liminality/shaders/include/volumetrics.glsl
@@ -1,25 +1,50 @@
 #version 330
 
-float cubeVolume(vec3 pos, vec3 size, vec3 look, float face) {
-    look *= face;
-    vec3 negative = (pos - size) / look;
-    vec3 positive = (pos + size) / look;
+#define volume(method, shape, ray, maxdepth) max(min(method(shape, ray, 1.0), maxdepth) - max(method(shape, ray, -1.0), 0.0))
+
+struct Ray {
+    vec3 pos;
+    vec3 look;
+};
+
+struct Cube {
+    vec3 pos;
+    vec3 size;
+};
+
+float cubeVolume(Cube cube, Ray ray, float face) {
+    vec3 relativePos = cube.pos - ray.pos;
+
+    ray.look *= face;
+    vec3 negative = (relativePos - cube.size) / ray.look;
+    vec3 positive = (relativePos + cube.size) / ray.look;
     float dist = -1.0 / 0.0;
 
-    if (look.x > 0.0 && negative.x > dist) dist = negative.x;
-    if (look.y > 0.0 && negative.y > dist) dist = negative.y;
-    if (look.z > 0.0 && negative.z > dist) dist = negative.z;
+    if (ray.look.x > 0.0 && negative.x > dist) dist = negative.x;
+    if (ray.look.y > 0.0 && negative.y > dist) dist = negative.y;
+    if (ray.look.z > 0.0 && negative.z > dist) dist = negative.z;
 
-    if (look.x < 0.0 && positive.x > dist) dist = positive.x;
-    if (look.y < 0.0 && positive.y > dist) dist = positive.y;
-    if (look.z < 0.0 && positive.z > dist) dist = positive.z;
+    if (ray.look.x < 0.0 && positive.x > dist) dist = positive.x;
+    if (ray.look.y < 0.0 && positive.y > dist) dist = positive.y;
+    if (ray.look.z < 0.0 && positive.z > dist) dist = positive.z;
 
     return dist * face;
 }
 
-float sphereVolume(vec3 pos, float radius, vec3 look, float face) {
-    float n = dot(look, pos);
-    float len = length(pos);
-    float delta = n * n - len * len + radius * radius;
+struct Sphere {
+    vec3 pos;
+    float radius;
+};
+
+float sphereVolume(Sphere sphere, Ray ray, float face) {
+    vec3 relativePos = sphere.pos - ray.pos;
+    float n = dot(ray.look, relativePos);
+    float len = length(relativePos);
+    float delta = n * n - len * len + sphere.radius * sphere.radius;
     return n + face * delta;
 }
+
+struct Cone {
+    vec3 pos;
+    vec3 look;
+};
